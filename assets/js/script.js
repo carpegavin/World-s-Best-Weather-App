06089f9bdb21d3281a82ac313bcdfd29babf1d20
Gavin Phill <carpe.gavin@gmail.com>
2020-11-17 20:40:34 -0600

    attempted to add forecast and uv without success.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,7 @@ console.log("test");
 
 var APIKey = "105fb5cfa2c589eefd6b17a6b1f5f6c0";
 
-// event listener
-// $("#searchTerm").on("click", function() {
+//----------------GET FROM API----------------------------------------
 
 $.ajax({
     url: "http://api.openweathermap.org/data/2.5/weather?q=austin&appid=105fb5cfa2c589eefd6b17a6b1f5f6c0&units=imperial" ,
@@ -28,52 +27,72 @@ $.ajax({
   }).then(function(response) {
     console.log(response);
   });
+  
+  //--------GET USER INPUT FROM SEARCH BAR-----------------------
+  
   var queryParams = { "api-key": "105fb5cfa2c589eefd6b17a6b1f5f6c0" };
   queryParams.q = $("#searchBar")
     .val()
     .trim();
+//-----------------------------------------------------------------------
+    
 
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=austin&appid=105fb5cfa2c589eefd6b17a6b1f5f6c0&units=imperial";
-// Here we run our AJAX call to the OpenWeatherMap API
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-  
+    
+//--------whatever goes into here will show up--------------------
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=austin&appid=105fb5cfa2c589eefd6b17a6b1f5f6c0&units=imperial";
+      
+    
+// ----------AJAX call to the OpenWeatherMap API------------
+     $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
 
-  
-    // We store all of the retrieved data inside of an object called "response"
-    .then(function(response) {
+//-----------------ON CLICK----------------------------------------
+    $("#searchTerm").on("click", function(event) {
+     event.preventDefault();
+    clear()
 
-      // Log the queryURL
+//----------------?????????????----------------------------------------   
+    var queryURL = buildQueryURL();
+    $.ajax({
+      url:queryURL,
+      method:"GET"
+            
+    }).then(updatePage);
+
+// --------------Log the queryURL---------------------------
       console.log(queryURL);
 
-      // Log the resulting object
+//------------Log the resulting object-----------------------
       console.log(response);
 
-      // Transfer content to HTML
+// ------------Transfer content to HTML---------------------
       $(".city").html("<h1>" + response.name + " Weather Details</h1>");
       $(".wind").text("Wind Speed: " + response.wind.speed);
       $(".humidity").text("Humidity: " + response.main.humidity);
       $(".tempF").text("temperature: " + response.main.temperature);
+
+      //$(".uvIndex").text("        : " +                  );
+
+      //$("#forecast").text("       : " +                  );
       
-      // Convert the temp to fahrenheit
+// -------------Convert the temp to fahrenheit---------------
       var tempF = (response.main.temp);
 
-      // add temp content to html
+// --------------add temp content to html-------------------
       $(".temp").text("Temperature (K) " + response.main.temp);
       $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
 
-      // Log the data in the console as well
+// ------------Log the data in the console as well-----------
       console.log("Wind Speed: " + response.wind.speed);
       console.log("Humidity: " + response.main.humidity);
       console.log("Temperature (F): " + tempF);
       
     });
   
- 
-
+ });
     
 
 
